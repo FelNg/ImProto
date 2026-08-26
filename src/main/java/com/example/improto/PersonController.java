9d7d607c2ed10042418c60c92e7365b67ef51c90
Felicity Nguyen <felnguyenbusiness@gmail.com>
2026-08-26 13:10:34 -0400

    reorganized imports

--- src/main/java/com/example/improto/PersonController.java
+++ src/main/java/com/example/improto/PersonController.java
@@ -1,13 +1,15 @@
 package com.example.improto;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.context.annotation.*;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
